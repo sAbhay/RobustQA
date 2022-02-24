aac24cd196ea7ee25821ae0de66f0a4b6696a4e6
Abhay Singhal <abhayaaditsinghal@gmail.com>
2022-02-24 02:26:41 +0000

    changed discriminator to have constant hidden layer size

--- dann/ce/discriminator.py
+++ dann/ce/discriminator.py
@@ -5,11 +5,11 @@ class Discriminator(torch.nn.Module):
     def __init__(self, input_size=int(768*0.9), n_classes=4):
         super().__init__()
         self.net = torch.nn.Sequential(
-            torch.nn.Linear(input_size, input_size),
+            torch.nn.Linear(input_size, 768),
             torch.nn.ReLU(inplace=True),
-            torch.nn.Linear(input_size, input_size),
+            torch.nn.Linear(768, 768),
             torch.nn.ReLU(inplace=True),
-            torch.nn.Linear(input_size, n_classes)
+            torch.nn.Linear(768, n_classes)
         )
 
     def forward(self, z):
